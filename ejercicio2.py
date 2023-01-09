12091d7383b9b8fdc38c1c2cd5499633e873da56
aleandresss <114179514+aleandresss@users.noreply.github.com>
2023-01-09 16:27:32 +0100

    hago la lista con el nombre y el rango

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,5 +2,21 @@ class stromtropper:
     def __init__ (rango,nombre):
         self.rango = rango
         self.nombre = nombre 
+    print(stromtropper)
 
-    def __str__(self)
+#experimentacion 
+class stromtropper:
+    def __init__(nombre,rango):
+        self.nombre = nombre
+        self.rango = rango
+
+    def _str_ (self):
+        return self.nombre
+
+    def _str_ rango(self):
+        return self.rango
+
+Lista = [stromtropper("TK-8654,4"),stromtropper("TK-7564,7"),stromtropper("TK-230,2")]
+
+
+
---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+
+
+
+def __str__(self):
+        return "lo quiero mostrar"
+        return "El nombre es {},Rango{}".format(self.nombre,self.rango)
